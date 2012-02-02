f4eb1b72e23cdcf642854033045ddcd87a92ddb1
wadd0032 <deannawadden@rogers.com>
2012-02-02 14:49:32 -0500

    added function to indicate completed task

--- js/to-do.js
+++ js/to-do.js
@@ -1,16 +1,22 @@
-var lists = document.getElementById('lists');
+var list = document.getElementById('list');
 var task = document.getElementById('task');
 
 document.getElementById('add').addEventListener('click', function (ev) {
 
 	var newTask = document.createElement('li');
 		newTask.innerHTML = task.value;
-		lists.appendChild(newTask);
+		list.appendChild(newTask);
 	
 	console.log(newTask);
 
 }, false);
 
-
+list.addEventListener('click', function (e) {
+  if (e.target.className == 'done') {
+    e.target.className = '';
+  } else {
+    e.target.className = 'done';
+  }
+}, false);
 
 
